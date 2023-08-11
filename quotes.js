a42bf2e6f033917e73dcccc5d0174b23ff555e94
lfigil <50636010+lfigil@users.noreply.github.com>
2023-08-11 12:10:53 -0400

    Added UI functionality

---
+++ quotes.js
@@ -0,0 +1,83 @@
+const localQuotes = [
+    {
+      text: "The only limit to our realization of tomorrow will be our doubts of today.",
+      author: "Franklin D. Roosevelt"
+    },
+    {
+      text: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+      author: "Winston Churchill"
+    },
+    {
+      text: "The way to get started is to quit talking and begin doing.",
+      author: "Walt Disney"
+    },
+    {
+      text: "Don't watch the clock; do what it does. Keep going.",
+      author: "Sam Levenson"
+    },
+    {
+      text: "The future belongs to those who believe in the beauty of their dreams.",
+      author: "Eleanor Roosevelt"
+    },
+    {
+      text: "Life is what happens when you're busy making other plans.",
+      author: "John Lennon"
+    },
+    {
+      text: "In the end, it's not the years in your life that count. It's the life in your years.",
+      author: "Abraham Lincoln"
+    },
+    {
+      text: "The only way to do great work is to love what you do.",
+      author: "Steve Jobs"
+    },
+    {
+      text: "Believe you can and you're halfway there.",
+      author: "Theodore Roosevelt"
+    },
+    {
+      text: "The future depends on what you do today.",
+      author: "Mahatma Gandhi"
+    },
+    {
+      text: "Strive not to be a success, but rather to be of value.",
+      author: "Albert Einstein"
+    },
+    {
+      text: "You miss 100% of the shots you don't take.",
+      author: "Wayne Gretzky"
+    },
+    {
+      text: "Happiness is not something readymade. It comes from your own actions.",
+      author: "Dalai Lama"
+    },
+    {
+      text: "I attribute my success to this: I never gave or took any excuse.",
+      author: "Florence Nightingale"
+    },
+    {
+      text: "The only thing standing between you and your dream is the will to try and the belief that it is actually possible.",
+      author: "Joel Brown"
+    },
+    {
+      text: "The best time to plant a tree was 20 years ago. The second best time is now.",
+      author: "Chinese Proverb"
+    },
+    {
+      text: "Your time is limited, don't waste it living someone else's life.",
+      author: "Steve Jobs"
+    },
+    {
+      text: "Don't be afraid to give up the good to go for the great.",
+      author: "John D. Rockefeller"
+    },
+    {
+      text: "Success usually comes to those who are too busy to be looking for it.",
+      author: "Henry David Thoreau"
+    },
+    {
+      text: "The only person you are destined to become is the person you decide to be.",
+      author: "Ralph Waldo Emerson"
+    }
+  ];
+  
